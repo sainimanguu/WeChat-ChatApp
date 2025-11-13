e9b92833165cdd465a495aeda04a47615cd61512
Umang Saini <sainiumang505@gmail.com>
2025-11-13 15:45:06 +0530

    ui

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { use } from 'react'
+import assets, { userDummyData } from '../assets/assets.js'
+import { useNavigate } from 'react-router-dom'
 
-const Sidebar = () => {
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h1>sidebar</h1>
-        </div>
+        <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? 'max-md:hidden' : ''}`}>
+            <div div className='pb-5'>
+                <div className='flex justify-between items-center'>
+                    <img src={assets.logo} alt="logo" className='max-w-40' />
+                    <div className='relative py-2 group'>
+                        <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
+                        <div className='absolute top-full right-0 z-20 w-32 p-5 ronded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
+
+                            <p onClick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit</p>
+                            <hr className='my-2 border-t border-gray-500'></hr>
+                            <p className='cursor-pointer text-sm'>Logout</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='bg-[#282142] flex rounded-full items-center gap-2 py-3 px-4 mt-5'>
+                    <img src={assets.search_icon} alt="search" className='w-3' />
+                    <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search User...'></input>
+                </div>
+            </div>
+
+            <div className='flex flex-col'>
+                {userDummyData.map((user, index) => (
+                    <div>
+                        <img src={user?.profilePic || assets.avatar_icon} alt="pic" className='w-[35px] aspext-[1/1] rounded-full' />
+                        <div className='flex flex-col leading-3'>
+                            <p>{user.fullName}</p>
+                            {
+                                index < 3 ? <sapn>Online</sapn> : <span>Offline</span>
+                            }
+
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div >
     )
 }
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,18 @@
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
+import { useState } from 'react'
 
 const HomePage = () => {
+
     const [selectedUser, setSelectedUser] = useState(null);
 
     return (
         <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-            <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-2 relative'>
+            <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-2 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
                 <Sidebar />
                 <ChatContainer />
-                <ChatContainer />
-
+                <RightSidebar />
             </div>
         </div>
     )
