f29a519ddbaa7c271ec11b240332d48cb5fce649
Umang Saini <sainiumang505@gmail.com>
2025-11-25 22:24:27 +0530

    ui

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,13 +6,13 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
     const navigate = useNavigate();
 
     return (
-        <div className={`bg-[#8185B]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? 'max-md:hidden' : ''}`}>
+        <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? 'max-md:hidden' : ''}`}>
             <div div className='pb-5'>
                 <div className='flex justify-between items-center'>
                     <img src={assets.logo} alt="logo" className='max-w-40' />
                     <div className='relative py-2 group'>
                         <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
-                        <div className='absolute top-full right-0 z-20 w-32 p-5 ronded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
+                        <div className='absolute top-full right-0 z-20 w-32 p-5 ronded-md bg-[#282142] border-gray-600 text-gray-100 hidden group-hover:block'>
 
                             <p onClick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit</p>
                             <hr className='my-2 border-t border-gray-500'></hr>
