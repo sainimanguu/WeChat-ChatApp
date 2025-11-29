a7911428a6aa3c93f59d30a8162c3ce8d5d51350
Umang Saini <sainiumang505@gmail.com>
2025-11-29 09:47:07 +0530

    ui

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
                     <img src={assets.logo} alt="logo" className='max-w-40' />
                     <div className='relative py-2 group'>
                         <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
-                        <div className='absolute top-full right-0 z-20 w-32 p-5 ronded-md bg-[#282142] border-gray-600 text-gray-100 hidden group-hover:block'>
+                        <div className='absolute top-full right-0 z-20 w-32 p-5 ronded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
 
                             <p onClick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit</p>
                             <hr className='my-2 border-t border-gray-500'></hr>
