4c0dff091f2c51254af3ed47fd49cd7d86099446
Umang Saini <sainiumang505@gmail.com>
2025-12-05 18:55:50 +0530

    ui

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils'
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+
+    const scrollEnd = useRef()
+
+    useEffect(() => {
+        if (scrollEnd.current) {
+            scrollEnd.current.scrollIntoView({ behaviour: "smooth" })
+        }
+    }, [])
+
     return selectedUser ? (
         <div className='h-full overflow-scroll relative background-blur-lg'>
 
             {/* header */}
 
-            <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
+            <div className='flex items-center gap-3 py-3 mx-3₹ border-b border-stone-500'>
                 <img src={assets.profile_martin} alt='' className='w-8 rounded-full' />
                 <p className='flex-1 text-lg text-white flex items-center gap-2'>
                     Martin Johnson
@@ -19,17 +29,33 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
 
             {/* chat area */}
             <div className='flex flex-col h-[calc{100%-120px}] overflow-y-scroll p-3 pb-6'>
+
                 {messagesDummyData.map((msg, index) => (
-                    <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== "68028388838fg9" && "flex-row-reverse"}`}>
+                    <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"}`}>
                         {msg.image ? (
                             <img className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' src={msg.image} alt='' />
                         ) : (
-                            <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-voilet-500/50 text-wwhite ${msg.senderId === '68028388838fg9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+                            <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
                         )}
+                        <div className='text-center text-xs'>
+                            <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full' />
+                            <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
+                        </div>
+
                     </div>
                 ))}
+                <div ref={scrollEnd}></div>
             </div>
-        </div> 
+
+            <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+                <div>
+                    <input type="text" placeholder="Send a message" />
+                    <input type="file" id='image' />
+                </div>
+
+            </div>
+
+        </div>
     ) : (
         <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
             <img src={assets.logo_icon} alt='' className='max-w-16' />
