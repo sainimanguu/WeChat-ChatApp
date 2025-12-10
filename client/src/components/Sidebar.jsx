857fd5eecb2d8e8ef23ead5757a2e6571f49b20c
Umang Saini <sainiumang505@gmail.com>
2025-12-10 10:42:57 +0530

    ui

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -25,7 +25,7 @@ const Sidebar = () => {
         <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? 'max-md:hidden' : ''}`}>
             <div className='pb-5'>
                 <div className='flex justify-between items-center'>
-                    <img src={assets.logo} alt="logo" className='max-w-40' />
+                    <img src={assets.logo} alt="logo" className='max-w-30' />
                     <div className='relative py-2 group'>
                         <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
                         <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
